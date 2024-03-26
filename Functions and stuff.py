0d53523930e29022b0115fb7376fca5fadb70ff4
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-03-26 18:20:01 -0500

    Finished Functions and stuff.py
    This was a small little test using functions. Nothing too big.

--- Functions and stuff.py
+++ Functions and stuff.py
@@ -1,9 +1,8 @@
 def test():
     name = input("What is your name?: ")
     return "Hello %s, How is your day going?: " % name
-test()
 def after():
     test2 = test()
     day = input(test2)
-    return 
+    print("Its wonderful that your day was %s!!!" % (day))
 after()
