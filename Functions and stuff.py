1cd5cfb86e12efb3d5fcff20cef5f5fb64ba3f31
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-03-26 15:46:25 -0500

    Create Functions and stuff.py

---
+++ Functions and stuff.py
@@ -0,0 +1,9 @@
+def test():
+    name = input("What is your name?: ")
+    return "Hello %s, How is your day going?: " % name
+test()
+def after():
+    test2 = test()
+    day = input(test2)
+    return 
+after()
