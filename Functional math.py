d190ceeb3def69305530fa6ef2577b821ef2e6be
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-03-28 15:01:10 -0500

    Update Functional math.py Again

--- Functional math.py
+++ Functional math.py
@@ -36,3 +36,44 @@ while (x < 6):
             return "Incorrect"
     corrin()
     def collection():
+        if(x == 1):
+            a1 = a
+            b1 = b
+            c1 = c
+            pa1 = pa
+            ic = corrin()
+            x += 1
+        else:
+            if(x == 2):
+                a2 = a
+                b2 = b
+                c2 = c
+                pa2 = pa
+                ic2 = corrin()
+                x += 1
+            else:
+                if(x == 3):
+                    a3 = a
+                    b3 = b
+                    c3 = c
+                    pa3 = pa
+                    ic3 = corrin()
+                    x += 1
+                else:
+                    if(x == 4):
+                        a4 = a
+                        b4 = b
+                        c4 = c
+                        pa4 = pa
+                        ic4 = corrin()
+                        x += 1
+                    else:
+                        if(x == 5):
+                            a5 = a
+                            b5 = b
+                            c5 = c
+                            pa5 = pa
+                            ic5 = corrin()
+                            x += 1
+                        else:
+                            
