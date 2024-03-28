6899e4b0e301fe4daa8c18ab9130f3f50607094c
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-03-28 14:45:32 -0500

    Update Functional math.py

--- Functional math.py
+++ Functional math.py
@@ -1,35 +1,38 @@
 x = 1
 import pandas as More_pandas
 import random
+Name = input("What is your name?: ")
 def ran():
     for i in range(1):
         yield random.randint(10, 100)
 def rand():
     for i in range(1):
-        yield random.randint(10, 50)
+        yield random.randint(1, 50)
 def name():
     Name = input("What is your name?: ")
     return Name
 while (x < 6):
     for random_number in ran():
-        def number1():
-            a = random_number
-            return a
+        a = random_number
     for random_number in rand():
-        def number2():
-            b = random_number
-            return b
+        b = random_number
     def question():
-        a = number1
-        b = number2
-        return "Solve the following equation. %d * %d?:" % a, b
+        return "Solve the following equation. %s * %s?:" % (a, b)
     def answer():
-        a = number1
-        b = number2
-        c = a + b
+        c = a * b
         return c
     def ask():
         q = question()
         pa = int(input((q)))
-    ask()
-    
+        return pa
+    def corrin():
+        pa = ask()
+        c = answer()
+        if(pa == c):
+            print("Correct")
+            return "Correct"
+        else:
+            print("Incorrect, The answer was %d" % (c))
+            return "Incorrect"
+    corrin()
+    def collection():
