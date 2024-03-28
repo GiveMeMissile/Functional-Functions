cd09470f72ba86ce3d1f68521e2e6a138a7b705c
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-03-28 08:26:46 -0500

    Functional math.py Was born
    Functional math is currently not functional.

---
+++ Functional math.py
@@ -0,0 +1,22 @@
+x = 1
+import pandas as More_pandas
+import random
+def ran():
+    for i in range(1):
+        yield random.randint(10, 100)
+def rand():
+    for i in range(1):
+        yield random.randint(10, 50)
+def name():
+    Name = input("What is your name?: ")
+    return Name
+while (x < 6):
+    for random_number in ran():
+        def number1():
+            a = random_number
+            return a
+    for random_number in rand():
+        def number2():
+            b = random_number
+            return b
+    break
